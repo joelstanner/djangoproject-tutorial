b3f03181772f0d943d599a5bfcadccd31f71ed28
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-09-08 13:17:34 -0700

    part 2 done

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from polls.models import Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuesionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    date_hierarchy = 'pub_date'
+    
+admin.site.register(Question, QuesionAdmin)
