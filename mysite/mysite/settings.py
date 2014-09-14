156fc1a6c25e7a95e211573ce9e04594604eacb5
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-09-13 21:29:05 -0700

    ready to deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,6 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -58,16 +57,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'polls_django_db',
-        'USER': 'djangousr',
-        'PASSWORD': 'p82x4f7Q6u9^JaoG',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'polls_django_db',
+#        'USER': 'djangousr',
+#        'PASSWORD': 'p82x4f7Q6u9^JaoG',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -92,3 +91,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(PROJECT_PATH, 'static'),)
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
