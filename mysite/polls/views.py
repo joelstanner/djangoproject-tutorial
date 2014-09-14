86992e9a990eefaf8f62e3c745521bf430e62dc8
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-09-13 20:01:57 -0700

    migratin

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,10 +13,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        """Return the last five published questions (not including published
+        """Return the last twenty published questions (not including published
         in the future)."""
         return Question.objects.filter(
-            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:20]
 
     
 class DetailView(generic.DetailView):
